19f36ebcc8fd0964bad034cab3813561ac211fa7
Harlan Wilton <harlan@harlanzw.com>
2026-08-13 23:15:35 +1000

    feat: add Nuxt Wide Events (#12)

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -10,6 +10,7 @@ export default defineNuxtConfig({
   extends: ['./layers/admin', './layers/tools'],
 
   modules: [
+    '@harlan-zw/nuxt-wide-events',
     '@harlan-zw/nuxt-github-sponsors',
     '@nuxt/ui',
     '@nuxtjs/seo',
@@ -39,6 +40,21 @@ export default defineNuxtConfig({
       })
     },
   ],
+
+  wideEvents: {
+    request: true,
+    service: 'unhead.unjs.io',
+    fields: [
+      'auth.github.failed',
+      'cache.kind',
+      'cache.name',
+      'cache.operation',
+      'cache.outcome',
+      'toolAnalytics.fetchFailed',
+      'toolTracking.analyticsEngineWriteFailed',
+      'toolTracking.d1WriteFailed',
+    ],
+  },
 
   ui: {
     mdc: true,
